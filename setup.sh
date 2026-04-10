4ce242709e3b12472093777a76e2be9b1d9f5ee6
Koki Tanaka <67k.tanaka@gmail.com>
2026-04-10 16:10:05 +0900

    Set ROS_DOMAIN_ID in setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Setup unitree ros2 environment"
 source /opt/ros/humble/setup.bash
-#source $HOME/unitree_ros2/cyclonedds_ws/install/setup.bash
+export ROS_DOMAIN_ID=0
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
                             <NetworkInterface name="enp97s0" priority="default" multicast="default" />
